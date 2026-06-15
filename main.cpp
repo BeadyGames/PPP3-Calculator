f8df000858b512258d44c3f8002eb957c51b12ce
Nathan <beadygames@outlook.com>
2026-06-15 14:01:59 +0100

    Added the concept of Tokens to deal with expressions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 
-
 /* Program that can calculate complicated expressions using the console window */
 
 struct Token
@@ -10,49 +9,76 @@ struct Token
 	double value; // for numbers: a value
 
 	Token(char k) : kind(k), value(0) {} 
-	Token(char k, double val) : kind(k), value(val) {}    // we'll use '8' to represent a number
+	Token(char k, double val) : kind(k), value(val) {}    // we still need a "kind" to represent a number
 };
 
-std::vector<Token> tokens; // store tokens here
+Token get_token()
+{
+	char ch;
+	std::cin >> ch; // read a character from the input
+	switch (ch)
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '=':
+		case 'e': // 'e' for escape
+		return Token(ch); // return a token for the operator
+
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+		std::cin.putback(ch); // put the digit back into the input stream
+		double val;
+		std::cin >> val; // read the full number
+		return Token('8', val); // return a token for the number
+	default:
+		std::cerr << "Error: Invalid token!" << '\n';
+		return Token('?'); // return an invalid token
+	}
+}
+
+std::vector<Token> tokens;  // store tokens for the expression
 
 int main()
 {
-	double val = 0;
-	double val2 = 0;
-	char op = ' ';
 	std::cout << "Enter an expression (e.g., 3 + 4): ";
 
-	std::cin >> val; // Read initial value
-
-	while (std::cin >> op)
+	// Read tokens into the vector until the end of input
+	while (std::cin)
 	{
-		switch (op)
+		Token t = get_token(); // Get the next token
+		if (t.kind == 'e') // 'e' for escape
+			break;
+
+		switch (t.kind)
 		{
 		case '+':
-			std::cin >> val2;
-			val += val2;
+			tokens.push_back(t);
 			break;
 		case '-':
-			std::cin >> val2;
-			val -= val2;
+			tokens.push_back(t);
 			break;
 		case '*':
-			std::cin >> val2;
-			val *= val2;
+			tokens.push_back(t);
 			break;
 		case '/':
-			std::cin >> val2;
-			if (val2 != 0)
-				val /= val2;
-			else
-				std::cout << "Error: Division by zero!" << '\n';
+			tokens.push_back(t);
 			break;
 		case '=':
-			std::cout << "Result: " << val << '\n';
-			val = 0; // Reset for the next expression
-			std::cout << "Enter an expression (e.g., 3 + 4): ";
-			std::cin >> val; // Read initial value for the next expression
+			tokens.push_back(t);
+			break;
+		case '8': // number
+			tokens.push_back(t);
 			break;
 		}
+	}
+
+	/* Just a few snags - programming the calculator this way means we have to find a '*' or '/'
+	   do a sub-calculation, and then calculate '+' and '-' afterwards. */
+
+	for (const auto& t : tokens)
+	{
+		
 	}
 }
